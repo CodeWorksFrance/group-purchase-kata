4077ee4c024cc38f98e63df36835985e18ce9370
Alexis Ragot <a.ragot.alexis@gmail.com>
2022-11-22 10:20:01 +0100

    :sparkles: enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,14 +5,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
-      .setTitle('Group purchase API example')
-      .setDescription('The Group purchase API description')
-      .setVersion('1.0')
-      .addTag('Group purchase API')
-      .build();
+    .setTitle('Group purchase API example')
+    .setDescription('The Group purchase API description')
+    .setVersion('1.0')
+    .addTag('Group purchase API')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-doc', app, document);
 
   await app.listen(3000);
 }
